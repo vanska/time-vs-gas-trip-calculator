c3ce7567ce00fc17d0e58c60c05558d9496ba55f
Mikko <mikkovanska.com@gmail.com>
2021-06-05 13:32:20 +0300

    Removed Tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,20 @@
 const config = {
   // Project title. Used as SEO title and PWA name
-  title: 'Gatsby Starter Vadyan',
+  title: 'Time vs. Gas trip calculator PWA',
   // Project short name. Used by PWA
-  shortTitle: 'GS Vadyan',
+  shortTitle: 'Time vs. Gas',
   // Project description. Used in SEO meta tag and by PWA
-  description: 'A modern content-agnostic Gatsby starter',
+  description: 'Trip calculator comparing gas usage between different travel speeds',
   // Keywords describing the project. Used in SEO meta tag
-  keywords: ['gatsby', 'gatsby-starter', 'react', 'typescript', 'storybook', 'jest', 'pwa', 'graphql'],
+  keywords: ['gatsby', 'gatsby-starter', 'react', 'typescript', 'jest', 'pwa', 'graphql'],
   // Absolute deployment path (without trailing slash). Used as base URL in SEO meta tags
-  baseUrl: 'https://gatsby-starter-vadyan.netlify.app',
+  baseUrl: 'https://time-vs-gas.vanska.dev',
   // Site language. Added in html tag and PWA manifest
   lang: 'en',
   // Your username on Facebook. Used in SEO meta tags
-  facebookUsername: 'p1t1ch',
+  facebookUsername: '',
   // Your username on Twitter (without @). Used in SEO meta tags
-  twitterUsername: 'p1t1ch',
+  twitterUsername: '',
   // Path to main favicon. Recommended size: 512x512. Other sizes are generated automatically
   favicon: 'src/icons/app-logo.svg',
   // Theme color. Used as color of device toolbar in supported browsers
@@ -30,8 +30,8 @@ module.exports = {
     keywords: config.keywords,
     baseUrl: config.baseUrl,
     lang: config.lang,
-    facebookUsername: config.facebookUsername,
-    twitterUsername: config.twitterUsername,
+    // facebookUsername: config.facebookUsername,
+    // twitterUsername: config.twitterUsername,
   },
   plugins: [
     {
@@ -53,7 +53,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-react-helmet',
-    // 'gatsby-plugin-postcss',
     'gatsby-plugin-image',
     {
       resolve: 'gatsby-source-filesystem',
